a5c5c27d4f8b03a39a8f3e6183fabf2e66016c27
Jahfal102 <126544993+Jahfal102@users.noreply.github.com>
2023-12-13 16:13:22 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,44 @@
+import streamlit as st
+from keras.optimizers import Adam
+from PIL import Image
+import numpy as np
+from tensorflow.keras.models import load_model
+
+
+from util import classify, set_background
+
+# Set background
+set_background('./BG/bg.jpg')
+
+# Set title
+st.title('Pneumonia covid classification')
+
+# Set header
+st.header('Please upload a chest X-ray image')
+
+# Upload file
+file = st.file_uploader('', type=['jpeg', 'jpg', 'png'])
+
+# Load classifier
+model = load_model('./model/PMA4.h5')
+
+# Compile the model
+optimizer = Adam(lr=0.01)  # Adjust the learning rate as needed
+model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+
+# load class names
+with open('./model/labels.txt', 'r') as f:
+    class_names = [a[:-1].split(' ')[1] for a in f.readlines()]
+    f.close()
+
+# Display image
+if file is not None:
+    image = Image.open(file).convert('RGB')
+    st.image(image, use_column_width=True)
+
+  # classify image
+    class_name, conf_score = classify(image, model, class_names)
+
+    # write classification
+    st.write("## {}".format(class_name))
+    st.write("### score: {}%".format(int(conf_score * 1000) / 10))
